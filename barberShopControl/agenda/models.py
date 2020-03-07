5222309b7c55698cb81fb83bfea80370f0bc0dc2
Anderson Silva <anderson.silva@indigosoft.com.br>
2020-03-07 18:00:58 -0300

    banco e models teste funcionando

--- barberShopControl/agenda/models.py
+++ barberShopControl/agenda/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Agenda(models.Model):
+    data = models.CharField(max_length=10)
+    servico = models.CharField(max_length=500)
+    
+    def __str__(self):
+        return "Data: "+self.data+" | Serviço(s): ["+ self.servico+"]"      
--- barberShopControl/agenda/views.py
+++ barberShopControl/agenda/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def agenda(request):
+    return HttpResponse("Página de agendamentos")
+    
