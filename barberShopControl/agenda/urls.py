9207460a5ee004ec8e405d68f88ae251e76c5a5b
Anderson Silva <anderson.silva@indigosoft.com.br>
2020-03-08 16:45:27 -0300

    renderizando dados do banco no front

--- barberShopControl/agenda/urls.py
+++ barberShopControl/agenda/urls.py
@@ -1,9 +1,9 @@
-from django.urls import path
+from django.conf.urls import url
 
 from . import views
 
 urlpatterns = [
-    path('', views.agenda, name='agenda'),
+    url(r'^$', views.agenda, name='agenda'),
     
     
 ]   
--- barberShopControl/agenda/views.py
+++ barberShopControl/agenda/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Agenda
 
 # Create your views here.
 
 def agenda(request):
-    return render(request,'agenda/index.html')
+    agendas = Agenda.objects.order_by('data')
+    return render(request,'agenda/index.html', {'agendas': agendas})
     
